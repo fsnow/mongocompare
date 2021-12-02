080329ffa273a7c95d06d2e6f749f40a5574f0a4
Frank Snow <fsnow@fsnow.com>
2021-12-02 18:01:20 -0500

    Index comparison improvements, _id comparison
    Index comparison improvements, added _id comparison

--- mongocompare.go
+++ mongocompare.go
@@ -7,6 +7,7 @@ import (
 	"log"
 	"os"
 	"sort"
+	"strconv"
 
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
@@ -32,8 +33,10 @@ func main() {
 	flag.StringVar(&targetDatabase, "targetDatabase", "", "target database")
 	flag.StringVar(&targetCollName, "targetCollName", "", "target collection")
 
-	var randomSampleSize int
+	var randomSampleSize, firstIdsCount, lastIdsCount int
 	flag.IntVar(&randomSampleSize, "randomSampleSize", 100, "random sample size used for content comparison")
+	flag.IntVar(&firstIdsCount, "firstIdsCount", 100, "number of _id values to compare from beginning")
+	flag.IntVar(&lastIdsCount, "lastIdsCount", 100, "number of _id values to compare from end")
 
 	flag.Parse()
 
@@ -49,9 +52,6 @@ func main() {
 	stringFromEnvVar(&targetPassword, "TARGET_PASSWORD", "targetPassword")
 	stringFromEnvVar(&targetDatabase, "TARGET_DATABASE", "targetDatabase")
 	stringFromEnvVar(&targetCollName, "TARGET_COLLECTION", "targetCollName")
-
-	// TODO: set the values from env vars here. As it is, they are being set as
-	// default values. The only problem with that is that the values get printed in --help.
 
 	sourceCredential := options.Credential{
 		Username: sourceUsername,
@@ -83,8 +83,10 @@ func main() {
 	checkCountsResult := checkCounts(sourceCollection, targetCollection)
 	indexCompareResult := compareIndexes(sourceCollection, targetCollection)
 	sampleContentResult := compareSampleContent(sourceCollection, targetCollection, randomSampleSize)
+	compareFirstIdsResult := compareIds(sourceCollection, targetCollection, firstIdsCount, 1)
+	compareLastIdsResult := compareIds(sourceCollection, targetCollection, lastIdsCount, -1)
 
-	if checkCountsResult && indexCompareResult && sampleContentResult {
+	if checkCountsResult && indexCompareResult && sampleContentResult && compareFirstIdsResult && compareLastIdsResult {
 		fmt.Println("Passed all validation checks")
 		os.Exit(0)
 	} else {
@@ -99,6 +101,12 @@ func main() {
 		}
 		if !sampleContentResult {
 			exitVal += 4
+		}
+		if !compareFirstIdsResult {
+			exitVal += 8
+		}
+		if !compareLastIdsResult {
+			exitVal += 16
 		}
 		os.Exit(exitVal)
 	}
@@ -134,46 +142,106 @@ func checkCounts(sourceCollection *mongo.Collection, targetCollection *mongo.Col
 }
 
 func compareIndexes(sourceCollection *mongo.Collection, targetCollection *mongo.Collection) bool {
+	// get source indexes as bson.D
 	cursor, err := sourceCollection.Indexes().List(context.TODO())
 	if err != nil {
 		log.Fatal(err)
 	}
-	var sourceIndexes []bson.M
+	var sourceIndexes []bson.D
 	if err = cursor.All(context.TODO(), &sourceIndexes); err != nil {
 		panic(err)
 	}
 
-	// delete "background" key for comparison
+	// A map where the keys are index names and the values are the key document.
+	// We need to compare the key document as a bson.D to preserve the order.
+	// The default index name is a good proxy for order, but an edge case is where
+	// the index is manually named incorrectly.
+	srcNameKeyMap := map[string]bson.D{}
 	for _, index := range sourceIndexes {
+		var name string
+		var key bson.D
+		for _, kv := range index {
+			if kv.Key == "key" {
+				key = kv.Value.(bson.D)
+			} else if kv.Key == "name" {
+				name = kv.Value.(string)
+			}
+		}
+		srcNameKeyMap[name] = key
+	}
+
+	// Other than the comparison of the "key" document where order matters,
+	// we will compare index docs as a bson.M map
+	sourceIndexMaps := make([]bson.M, len(sourceIndexes))
+	for i, doc := range sourceIndexes {
+		sourceIndexMaps[i] = doc.Map()
+	}
+	// delete "background" key from source maps
+	for _, index := range sourceIndexMaps {
 		delete(index, "background")
 	}
 
+	// get target indexes as bson.D
 	cursor, err = targetCollection.Indexes().List(context.TODO())
 	if err != nil {
 		log.Fatal(err)
 	}
-	var targetIndexes []bson.M
+	var targetIndexes []bson.D
 	if err = cursor.All(context.TODO(), &targetIndexes); err != nil {
 		panic(err)
 	}
 
-	// delete "background" key for comparison
-	for _, index := range targetIndexes {
+	// convert target indexes to bson.M
+	targetIndexMaps := make([]bson.M, len(targetIndexes))
+	for i, doc := range targetIndexes {
+		targetIndexMaps[i] = doc.Map()
+	}
+	// delete "background" key from target maps
+	for _, index := range targetIndexMaps {
 		delete(index, "background")
 	}
 
-	// Sort the two slices of indexes by the name, since they can be in different orders coming
+	// name/key map for target indexes
+	tgtNameKeyMap := map[string]bson.D{}
+	for _, index := range targetIndexes {
+		var name string
+		var key bson.D
+		for _, kv := range index {
+			if kv.Key == "key" {
+				key = kv.Value.(bson.D)
+			} else if kv.Key == "name" {
+				name = kv.Value.(string)
+			}
+		}
+		tgtNameKeyMap[name] = key
+	}
+
+	// Sort the two slices of maps by the name, since they can be in different orders coming
 	// from Indexes().List().
 	// TODO: What determines the order? I deleted one and recreated and it came last in the list.
-	sort.SliceStable(sourceIndexes, func(i, j int) bool {
-		return sourceIndexes[i]["name"].(string) < sourceIndexes[j]["name"].(string)
+	sort.SliceStable(sourceIndexMaps, func(i, j int) bool {
+		return sourceIndexMaps[i]["name"].(string) < sourceIndexMaps[j]["name"].(string)
 	})
-	sort.SliceStable(targetIndexes, func(i, j int) bool {
-		return targetIndexes[i]["name"].(string) < targetIndexes[j]["name"].(string)
+	sort.SliceStable(targetIndexMaps, func(i, j int) bool {
+		return targetIndexMaps[i]["name"].(string) < targetIndexMaps[j]["name"].(string)
 	})
-	isEqual := cmp.Equal(sourceIndexes, targetIndexes)
 
-	if !isEqual {
+	// compare the indexes as sorted slices of maps, minus the deleted "background" vals
+	isEqual := cmp.Equal(sourceIndexMaps, targetIndexMaps)
+
+	// compare the index keys as bson.D because order matters
+	keysMatch := true
+	if isEqual {
+		for name, srcKey := range srcNameKeyMap {
+			tgtKey := tgtNameKeyMap[name]
+			if !cmp.Equal(srcKey, tgtKey) {
+				keysMatch = false
+				fmt.Println("Index key comparison failed on " + name)
+			}
+		}
+	}
+
+	if !isEqual || !keysMatch {
 		fmt.Println("Indexes are not the same. Source indexes:")
 		fmt.Println(sourceIndexes)
 		fmt.Println("Target indexes:")
@@ -186,6 +254,9 @@ func compareIndexes(sourceCollection *mongo.Collection, targetCollection *mongo.
 }
 
 func compareSampleContent(sourceCollection *mongo.Collection, targetCollection *mongo.Collection, randomSampleSize int) bool {
+	if randomSampleSize <= 0 {
+		return true
+	}
 	pipeline := bson.A{bson.D{{"$sample", bson.D{{"size", randomSampleSize}}}}}
 	cursor, err := sourceCollection.Aggregate(context.TODO(), pipeline)
 	if err != nil {
@@ -212,6 +283,72 @@ func compareSampleContent(sourceCollection *mongo.Collection, targetCollection *
 
 	if matches < randomSampleSize {
 		return false
+	}
+	return true
+}
+
+func compareIds(sourceCollection *mongo.Collection, targetCollection *mongo.Collection, ids int, direction int) bool {
+	if ids <= 0 {
+		return true
+	}
+	query := bson.D{}
+	projection := bson.D{{"_id", 1}}
+	sort := bson.D{{"_id", direction}}
+	srcCursor, err := sourceCollection.Find(context.TODO(), query, options.Find().SetProjection(projection), options.Find().SetSort(sort))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer srcCursor.Close(context.TODO())
+	tgtCursor, err := targetCollection.Find(context.TODO(), query, options.Find().SetProjection(projection), options.Find().SetSort(sort))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer tgtCursor.Close(context.TODO())
+
+	count := 0
+	for {
+		srcHasNext := srcCursor.Next(context.TODO())
+		tgtHasNext := tgtCursor.Next(context.TODO())
+
+		var srcDoc, tgtDoc bson.M
+		var src_id, tgt_id interface{}
+		if srcHasNext {
+			srcCursor.Decode(&srcDoc)
+			src_id = srcDoc["_id"]
+		}
+		if tgtHasNext {
+			tgtCursor.Decode(&tgtDoc)
+			tgt_id = tgtDoc["_id"]
+		}
+
+		if srcHasNext && tgtHasNext {
+			//fmt.Println("Src: " + src_id + ", Tgt: " + tgt_id)
+			if !cmp.Equal(src_id, tgt_id, cmp.AllowUnexported(primitive.Decimal128{})) {
+				fmt.Print("_id mismatch, iteration " + strconv.Itoa(count) + ", sort " + strconv.Itoa(direction))
+				fmt.Print(", Source _id: ")
+				fmt.Print(src_id)
+				fmt.Print(", Target _id: ")
+				fmt.Println(tgt_id)
+				return false
+			}
+		} else if srcHasNext {
+			fmt.Print("Target collection is missing _id ")
+			fmt.Print(src_id)
+			fmt.Println(" (end)")
+			return false
+		} else if tgtHasNext {
+			fmt.Print("Source collection is missing _id ")
+			fmt.Print(tgt_id)
+			fmt.Println(" (end)")
+			return false
+		} else {
+			break
+		}
+
+		count++
+		if count >= ids {
+			break
+		}
 	}
 	return true
 }
